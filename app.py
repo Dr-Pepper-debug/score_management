c7b4c8c8b36caa3133984a0fd267027beb0c0e37
Dr-Pepper-debug <takumikobayashi0704@gmail.com>
2022-12-10 23:58:15 +0900

    [Add] db

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-﻿from flask import Flask, render_template
+﻿from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///score.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(30), nullable=False)
+    title = db.Column(db.String(30), nullable=False)
+    composer = db.Column(db.String(30), nullable=False)
+    arranger = db.Column(db.String(30), nullable=False)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
+
+    else:
+        number = request.form.get('number')
+        title = request.form.get('title')
+        composer = request.form.get('composer')
+        arranger = request.form.get('arranger')
+        
+        new_post = Post(number=number, title=title, composer=composer, arranger=arranger)
+
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/add')
+def create():
+    return render_template('add.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
