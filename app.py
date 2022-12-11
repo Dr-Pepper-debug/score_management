8242972fa43f3bca7893a2c920f345072eca3da0
Dr-Pepper-debug <takumikobayashi0704@gmail.com>
2022-12-11 15:57:07 +0900

    add edit

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ db = SQLAlchemy(app)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.String(30), nullable=False)
-    title = db.Column(db.String(30), nullable=False)
-    composer = db.Column(db.String(30), nullable=False)
-    arranger = db.Column(db.String(30), nullable=False)
+    number = db.Column(db.String(30))
+    title = db.Column(db.String(30))
+    composer = db.Column(db.String(30))
+    arranger = db.Column(db.String(30))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -24,9 +24,9 @@ def index():
         composer = request.form.get('composer')
         arranger = request.form.get('arranger')
         
-        new_post = Post(number=number, title=title, composer=composer, arranger=arranger)
+        new_score = Post(number=number, title=title, composer=composer, arranger=arranger)
 
-        db.session.add(new_post)
+        db.session.add(new_score)
         db.session.commit()
         return redirect('/')
 
@@ -56,11 +56,41 @@ def score_list():
     scores = Post.query.all()
     return render_template('score_list.html', scores=scores)
 
+@app.route('/score_search', methods=['GET', 'POST'])
+def score_search():
+    if request.method == 'GET':
+        return render_template('score_search.html')
+    if request.method == 'POST':
+        form_title = request.form.get("title")  # str
+        search_results = db.session.query(Post).filter(Post.title == form_title)
+        return render_template('result.html', search_results=search_results)
+
+        # scores = Post.query.all()
+        # return render_template('score_search.html', scores=scores)
+
 @app.route('/scores/<int:id>/delete', methods=['POST'])  
 def score_delete(id):  
     score = Post.query.get(id)   
     db.session.delete(score)  
     db.session.commit()  
+    return redirect(url_for('score_list'))
+
+@app.route('/scores/<int:id>/edit', methods=['GET'])
+def score_edit(id):
+    # 編集ページ表示用
+    score = Post.query.get(id)
+    return render_template('score_edit.html', score=score)
+
+@app.route('/scores/<int:id>/update', methods=['POST'])
+def score_update(id):
+    score = Post.query.get(id)  # 更新するデータをDBから取得
+    score.number = request.form.get("number")  # str
+    score.title = request.form.get("title")  # str
+    score.composer = request.form.get("composer")  # str
+    score.arranger = request.form.get("arranger")  # str
+
+    db.session.merge(score)
+    db.session.commit()
     return redirect(url_for('score_list'))
 
 if __name__ == "__main__":
